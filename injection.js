9451ff4dbdb411d6519e845c460a78294bc73c9e
hhjinks <benteinvathnethomassen@gmail.com>
2018-03-23 11:19:06 +0100

    changed styling and completed injection script

--- injection.js
+++ injection.js
@@ -1,5 +1,10 @@
 document.body.appendChild(document.createElement('app-root'));
 
+link = document.createElement('link');
+link.href = "https://fonts.googleapis.com/icon?family=Material+Icons";
+link.rel = "stylesheet";
+document.body.appendChild(link);
+
 script = document.createElement('script');
 script.async = false;
 script.src = "http://localhost:4200/inline.bundle.js";
