9fa63aacd8459c1620dc1c7f1d75ba4fa46e552b
Christian Normando Palec Iversen <149884@stud.hvl.no>
2018-05-16 11:51:48 +0200

    added clear history

--- src/app/components/titlebar/titlebar.component.ts
+++ src/app/components/titlebar/titlebar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
 import { DataManagerService } from '../../services/datamanager/datamanager.service';
+import { AlternativButtonLogicService } from '../../services/alternativbuttonlogic/alternativ-button-logic.service';
 
 @Component({
   selector: 'titlebar',
@@ -9,13 +10,20 @@ import { DataManagerService } from '../../services/datamanager/datamanager.servi
 })
 export class TitlebarComponent implements OnInit {
 
-  constructor(private data:DataManagerService) { }
+  constructor(private data:DataManagerService, private altHandler:AlternativButtonLogicService) { }
 
   ngOnInit() {}
 
   // Toogle chatbox 
   toggleChatBox($event) {
     this.data.toggleChatBox();
+  }
+
+
+  clearHistory($event) {
+    $event.stopPropagation();
+    this.data.clearMessages();
+    this.altHandler.deleteAllAlternatives();
   }
 
   // Close 
